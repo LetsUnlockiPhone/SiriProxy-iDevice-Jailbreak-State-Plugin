d99385e37c5f3137ba4b00eeedfaa41ae688e22a
LetsUnlockiPhone <letsunlockiphone@gmail.com>
2012-06-29 13:32:05 -0400

    2

--- lib/siriproxy-jailbreak.rb
+++ lib/siriproxy-jailbreak.rb
@@ -12,7 +12,7 @@ class SiriProxy::Plugin::Jailbreak < SiriProxy::Plugin
 
 	def get_jailbrake(device, version, ios)
 		url = "http://www.letsunlockiphone.com/jailbreak.php?device=#{device}&version=#{version}&ios=#{ios}"
-		page = HTTParty.get(path).body
+		page = HTTParty.get(uri).body
 
 		#parse and return content here
 
